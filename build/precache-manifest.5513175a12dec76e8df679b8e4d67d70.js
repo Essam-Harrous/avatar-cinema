a7d68dae8b0b852f8c496d33323db408da4c585b
essam <esamfarg91@gmail.com>
2020-01-12 18:19:53 +0100

    make it better

---
+++ build/precache-manifest.5513175a12dec76e8df679b8e4d67d70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bcb561002230ce36ec10b83a3d3763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f93f6d7f17ab2375745",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "32042e948dbdc76e1165",
+    "url": "/static/css/main.4c15a2c0.chunk.css"
+  },
+  {
+    "revision": "6f93f6d7f17ab2375745",
+    "url": "/static/js/2.75f2d429.chunk.js"
+  },
+  {
+    "revision": "70b1d5a76d38abf703f31cc679f8cd49",
+    "url": "/static/js/2.75f2d429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32042e948dbdc76e1165",
+    "url": "/static/js/main.310d4d9c.chunk.js"
+  },
+  {
+    "revision": "b44160213746cc7bce8b",
+    "url": "/static/js/runtime-main.0c0a83cf.js"
+  }
+]);
